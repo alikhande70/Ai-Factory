aa0c0b3ff47a63b31b6030ed80c641d09f9d8be8
alikhande70 <alikhande70@gmail.com>
2026-08-25 14:23:30 +0330

    phase8: expose durable organizational memory store

--- factory/memory/__init__.py
+++ factory/memory/__init__.py
@@ -2,10 +2,13 @@
 
 from .contracts import MemoryCandidate, MemoryPromotionDecision, OrganizationalMemoryEntry
 from .promotion import MemoryPromotionGate
+from .store import MemoryRecord, SQLiteOrganizationalMemoryStore
 
 __all__ = [
     "MemoryCandidate",
     "MemoryPromotionDecision",
     "OrganizationalMemoryEntry",
     "MemoryPromotionGate",
+    "MemoryRecord",
+    "SQLiteOrganizationalMemoryStore",
 ]
