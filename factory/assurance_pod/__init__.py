79f33245bd1d2211c11de9a5557eca072292eaa4
alikhande70 <alikhande70@gmail.com>
2026-08-25 12:26:19 +0330

    Phase 5: export assurance lifecycle contracts

--- factory/assurance_pod/__init__.py
+++ factory/assurance_pod/__init__.py
@@ -3,20 +3,32 @@
 from .contracts import (
     ASSURANCE_ROLES,
     ASSURANCE_SEVERITIES,
+    AcceptanceCoverage,
     AssuranceDecision,
     AssuranceFinding,
     AssuranceReport,
 )
 from .coordinator import AssurancePodCoordinator
+from .lifecycle import (
+    AssuranceCycleRecord,
+    AssuranceLifecycle,
+    RemediationRequest,
+    integration_fingerprint,
+)
 from .validator import AssuranceValidator
 from .workers import AssuranceWorker
 
 __all__ = [
     "ASSURANCE_ROLES",
     "ASSURANCE_SEVERITIES",
+    "AcceptanceCoverage",
     "AssuranceDecision",
     "AssuranceFinding",
     "AssuranceReport",
+    "AssuranceCycleRecord",
+    "AssuranceLifecycle",
+    "RemediationRequest",
+    "integration_fingerprint",
     "AssurancePodCoordinator",
     "AssuranceValidator",
     "AssuranceWorker",
